992bdd76f2e6f3772b9233e7adaf29f185b0650e
shukrishire <shukrishire28@gmail.com>
2023-07-14 06:20:45 +1200

    Newname + content

---
+++ Scrimba_Course_B.py
@@ -0,0 +1,9 @@
+# Number 7,10, 13, 16 Scrimba 
+# Exercise 7
+item_name = 'hydrid car'
+item_price = float(5000.97)
+inventory = int(10)
+is_in_inventory = True
+print(item_name, item_price, inventory,is_in_inventory )
+
+# Exercise 10
